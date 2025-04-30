339d8d5b721fd9c6f87012ea7c91fe424620cbc2
Rafael Vercezi Scatolin <rafael.scatolin@orguel.com.br>
2025-04-30 15:38:30 -0300

    update

--- CreateGraphOCR RV01.py
+++ CreateGraphOCR RV01.py
@@ -9,6 +9,70 @@
 
 
 
+class Graph(BaseGraph):
+    __edge_attributes = {
+                            "parallel": 0,
+                            "colinear": 0,
+                            "perpendicular_distance": 0,
+                            "overlap_ratio": 0,
+                            "point_intersection": 0,
+                            "segment_intersection": 0,
+                            "angle_difference": 0
+                        }
+    
+    def __init__(self, dataframe):
+        self.__dataframe = dataframe
+        
+        coordinates_x = dataframe[['start_x', 'end_x', 'character_insertion_x']]
+        coordinates_y = dataframe[['start_y', 'end_y', 'character_insertion_y']]
+        
+        normalizedCoordinates = dataframe[['start_x', 'start_y', 'end_x', 'end_y']].copy()
+        normalizedCoordinates[['start_x', 'end_x']] = (normalizedCoordinates[['start_x', 'end_x']] - coordinates_x.values.mean()) / coordinates_x.values.std()
+        normalizedCoordinates[['start_y', 'end_y']] = (normalizedCoordinates[['start_y', 'end_y']] - coordinates_y.values.mean()) / coordinates_y.values.std()
+        
+        normalizedAngles = numpy.column_stack((numpy.sin(dataframe['angle']), numpy.cos(dataframe['angle'])))
+        
+        if dataframe['character_id'].nunique() == 1 and dataframe['character_id'].iloc[0] != 0:
+            normalizedLengths = dataframe[['length']] / 176.53671467337412
+            normalizedCharacterHeigth = dataframe[['character_height']] / 176.53671467337412
+            normalizedCharacterWidth = dataframe[['character_width']] / 176.53671467337412
+        else:
+            lengths = dataframe[['length', 'character_height', 'character_width']].copy().values
+            if lengths.max() - lengths.min() > 1e-6:
+                normalizedLengths = (dataframe[['length']] - lengths.min()) / (lengths.max() - lengths.min())
+                normalizedCharacterHeigth = (dataframe[['character_height']] - lengths.min()) / (lengths.max() - lengths.min())
+                normalizedCharacterWidth = (dataframe[['character_width']] - lengths.min()) / (lengths.max() - lengths.min())
+            
+            
+
+        
+        
+        
+        normalizedLengths = dataframe[['length']] / 176.53671467337412
+        
+        cluster = dataframe.groupby("cluster_id")
+        
+        self.classificationLabels = {
+            "label": cluster['cluster_label'].first().values,
+            "font": cluster['cluster_font'].first().values
+        }
+        self.regressionTargets = {
+            "height": cluster['cluster_height'].first().values / 176.53671467337412,
+            "rotation": cluster['cluster_rotation'].first().values,
+            "insertion_x": ((cluster['cluster_insertion_x'].first() - coordinates_x.values.mean()) / coordinates_x.values.std()).values,
+            "insertion_y": ((cluster['cluster_insertion_y'].first() - coordinates_y.values.mean()) / coordinates_y.values.std()).values
+        }
+        self.nodesAttributes = numpy.hstack([normalizedCoordinates.values, normalizedAngles, normalizedLengths.values])
+        self.edges: List[Tuple[int, int]] = []
+        self.edgesAttributes: List[List[float]] = []
+
+
+
+
+
+
+
+
 
 
 
@@ -20,8 +84,6 @@ def __rotate_and_scale(x, y, angle, scale):
     x_aligned = x * scale * math.cos(angle_rad) - y * scale * math.sin(angle_rad)
     y_aligned = y * scale * math.sin(angle_rad) + y * scale * math.cos(angle_rad)
     return x_aligned, y_aligned
-
-
 
 def CreateGraph(dxf_file, character_positions, rotation=0, scale=1.0):
     # Filter only rows for this dxf file
@@ -37,9 +99,9 @@ def CreateGraph(dxf_file, character_positions, rotation=0, scale=1.0):
         aligned_insertion_x, aligned_insertion_y = __rotate_and_scale(insertion_x, insertion_y, rotation, scale)
         characterPositions.at[i, 'insertion'] = (aligned_insertion_x, aligned_insertion_y)
         
-        characterPositions.at[idx, 'height'] = row['height'] * scale
-        characterPositions.at[idx, 'width'] = row['width'] * scale
-        characterPositions.at[idx, 'rotation'] = (row['rotation'] + math.radians(rotation)) % (2 * math.pi)
+        characterPositions.at[i, 'height'] = row['height'] * scale
+        characterPositions.at[i, 'width'] = row['width'] * scale
+        characterPositions.at[i, 'rotation'] = (row['rotation'] + math.radians(rotation)) % (2 * math.pi)
 
     # Prepare spatial index
     polygons = list(characterPositions['bbox'])
@@ -113,4 +175,11 @@ def CreateGraph(dxf_file, character_positions, rotation=0, scale=1.0):
         dataframe.append(label)
 
     dataframe = pandas.DataFrame(dataframe)
+    
+    graph = Graph(dataframe)
+    
+    
+    
+    
+    
     return dataframe
